afd739b15df23b25b603b7844d4f992df7522d73
Jahimees <JerryMous1901@gmail.com>
2020-08-09 11:07:08 +0300

    Ишруля

--- main/second/Game.java
+++ main/second/Game.java
@@ -1,46 +1,44 @@
 package second;
+
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    String command;
+        Scanner scanner = new Scanner(System.in);
+        String command;
 
+        System.out.println("Вы начинаете новую игру");
+        Player player = new Player();
 
-    System.out.println("Вы начинаете новую игру");
-    Player player = new Player();
-
-    while (player.getAlive()) {
-        command = scanner.nextLine();
-        switch (command) {
-            case "Поставить блок": {
-                player.putBlock();
-                break;
-            }
-            case "Добыть блок": {
-                player.getBlock();
-                break;
-            }
-            case "Кушать": {
-                player.eat();
-                break;
-            }
-            case "Убить моба": {
-                player.fight();
-                break;
-            }
-            case "Помощь": {
-                System.out.println(" - Добыть блок \n - Поставить блок \n - Кушать \n - Убить моба ");
-                break;
-            }
-            case "Выход":{
-                System.exit(0);
-                break;
+        while (player.getAlive()) {
+            command = scanner.nextLine();
+            switch (command) {
+                case "Поставить блок": {
+                    player.putBlock();
+                    break;
+                }
+                case "Добыть блок": {
+                    player.getBlock();
+                    break;
+                }
+                case "Кушать": {
+                    player.eat();
+                    break;
+                }
+                case "Убить моба": {
+                    player.fight();
+                    break;
+                }
+                case "Помощь": {
+                    System.out.println(" - Добыть блок \n - Поставить блок \n - Кушать \n - Убить моба ");
+                    break;
+                }
+                case "Выход": {
+                    System.exit(0);
+                    break;
+                }
             }
         }
     }
-
-    }
-
 }
